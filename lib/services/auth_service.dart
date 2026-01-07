c51113fede28f98d33885d3990ad6e5741ac326b
ronSiuuuu <mehedihasanakash1717@gmail.com>
2026-01-07 19:08:19 +0600

    created authService

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,25 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final supabase = Supabase.instance.client;
+
+class AuthService {
+  Future<AuthResponse> signUp(String email, String password) async {
+    return await supabase.auth.signUp(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<AuthResponse> signIn(String email, String password) async {
+    return await supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    await supabase.auth.signOut();
+  }
+
+  User? get currentUser => supabase.auth.currentUser;
+}
